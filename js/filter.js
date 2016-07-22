19632458a206d2ec44d0f04827244ebec7c1bc91
sunquan8094 <sunquan8094@gmail.com>
2016-07-21 23:55:28 -1000

    Fixed filter script to allow for more types of content

--- js/filter.js
+++ js/filter.js
@@ -12,14 +12,38 @@ function removeElements(e) {
 	e.fadeOut("fast");
 }
 
+function elementsToTop(e, before) {
+	e.insertBefore(before);
+}
+
 document.addEventListener("DOMNodeInserted", function() {
-	var res = chrome.storage.sync.get("iDunLikeREs", function (items) {
-			var regexes = items["iDunLikeREs"];
-			for (var r = 0; r < regexes.length; r++) {
-		 		var e = $("div._401d:regex(\'" + regexes[r] + "\')");
-				var h = $("div._4-u2:regex(\'" + regexes[r] + "\')");
+	var res = chrome.storage.sync.get({"iDunLikeREs": [], "iDunLikeREsLikey": []}, function (items) {
+			var _regexes = items["iDunLikeREs"];
+			for (var r = 0; r < _regexes.length; r++) {
+		 		var e = $("div._4ikz:regex(\'" + _regexes[r] + "\')"); // Posts
+				var h = $("div._3u1._gli._1c30:regex(\'" + _regexes[r] + "\')"); // Results Page Pages
+				var f = $("div._401d:regex(\'" + _regexes[r] + "\')");
+				var n = $("div._4qjp:regex(\'" + _regexes[r] + "\')");
+				var t = $("li._5my2:regex(\'" + _regexes[r] + "\')"); // Trending
 				removeElements(e);
 				removeElements(h);
+				removeElements(f);
+				removeElements(n);
+				removeElements(t);
 			}
+
+			var _regexes_likey = items["iDunLikeREsLikey"];
+    	for (var r = 0; r < _regexes_likey.length; r++) {
+				var e = $("div._4ikz:regex(\'" + _regexes_likey[r] + "\')"); // Posts
+				var h = $("div._3u1._gli._1c30:regex(\'" + _regexes_likey[r] + "\')"); // Results Page Pages
+			  var f = $("div._401d:regex(\'" + _regexes_likey[r] + "\')");
+				var n = $("div._4qjp:regex(\'" + _regexes_likey[r] + "\')");
+				var t = $("li._5my2:regex(\'" + _regexes_likey[r] + "\')");
+			}
+			elementsToTop(e, 'div._4ikz:first');
+			elementsToTop(h, 'div._3u1._gli._1c30:first');
+			elementsToTop(f, 'div._401d:first');
+			elementsToTop(n, 'div._4qjp:first');
+			elementsToTop(t, 'li._5my2:first');
 	});
 });
--- js/options.js
+++ js/options.js
@@ -1,57 +1,123 @@
+// begin no-likey
 function addToList(str) {
 	var l = document.createElement("li"), rmv = document.createElement("button"), edt = document.createElement("button");
 	rmv.innerHTML = "Remove";
 	edt.innerHTML = "Edit";
+	rmv.setAttribute("class","rmv-no-likey");
+  edt.setAttribute("class","edt-no-likey");
 	l.appendChild(document.createTextNode(str));
 	l.appendChild(edt);
 	l.appendChild(rmv);
-	l.setAttribute("class", "regexp");
+	l.setAttribute("class", "regexp-no-likey");
 	document.getElementById("list").appendChild(l);
 }
 
 function addRegex() {
-	if (document.getElementById("regex").value !== "") {
-		addToList(document.getElementById("regex").value);
+	if (document.getElementById("regex-no-likey").value !== "") {
+		addToList(document.getElementById("regex-no-likey").value);
 	}
-	document.getElementById("regex").value = "";
+	document.getElementById("regex-no-likey").value = "";
 }
+
+function editElement(e) {
+	document.getElementById("regex-no-likey").value = e.target.parentNode.innerHTML.substring(0, e.target.parentNode.innerHTML.indexOf("<"));
+	removeElement(e);
+}
+// end no-likey
+
+// begin likey
+function addToListLikey(str) {
+	var l = document.createElement("li"), rmv = document.createElement("button"), edt = document.createElement("button");
+	rmv.innerHTML = "Remove";
+	edt.innerHTML = "Edit";
+	rmv.setAttribute("class","rmv-likey");
+  edt.setAttribute("class","edt-likey");
+	l.appendChild(document.createTextNode(str));
+	l.appendChild(edt);
+	l.appendChild(rmv);
+	l.setAttribute("class", "regexp-likey");
+	document.getElementById("list-likey").appendChild(l);
+}
+
+function addRegexLikey() {
+	if (document.getElementById("regex-likey").value !== "") {
+		addToListLikey(document.getElementById("regex-likey").value);
+	}
+	document.getElementById("regex-likey").value = "";
+}
+
+function editElementLikey(e) {
+	document.getElementById("regex-likey").value = e.target.parentNode.innerHTML.substring(0, e.target.parentNode.innerHTML.indexOf("<"));
+	removeElement(e);
+}
+// end likey
 
 function removeElement(e) {
 	e.target.parentNode.parentNode.removeChild(e.target.parentNode);
 }
 
-function editElement(e) {
-	document.getElementById("regex").value = e.target.parentNode.innerHTML.substring(0, e.target.parentNode.innerHTML.indexOf("<"));
-	removeElement(e);
-}
-
 function saveOptions() {
-	var regexps = document.getElementsByClassName("regexp"), regexes = [];
+	var regexps = document.getElementsByClassName("regexp-no-likey"), regexes = [];
+	var regexps_likey = document.getElementsByClassName("regexp-likey"), regexes_likey = [];
 	for (var p = 0; p < regexps.length; p++) {
 		regexes.push(regexps[p].innerHTML.substring(0, regexps[p].innerHTML.indexOf("<")));
 	}
-	chrome.storage.sync.set({ "iDunLikeREs": regexes }, function(items) {
-		window.alert("Options saved!");
-	});
+	for (var p = 0; p < regexps_likey.length; p++) {
+		regexes_likey.push(regexps_likey[p].innerHTML.substring(0, regexps_likey[p].innerHTML.indexOf("<")));
+	}
+	if (!commonValue(regexes, regexes_likey)) {
+		chrome.storage.sync.set({ "iDunLikeREs": regexes, "iDunLikeREsLikey": regexes_likey}, function(items) {
+			window.alert("Options saved!");
+		});
+	}
+  else {
+		window.alert("Same value in both lists detected!");
+	}
+}
+
+function commonValue(arr1, arr2) {
+	var a = arr1.slice(0), b = arr2.slice(0), r = false;
+	a.sort(function(c,d) { return c.localeCompare(d); });
+	b.sort(function(c,d) { return c.localeCompare(d); });
+	while (a.length > 0 && b.length > 0) {
+		if (a[0].localeCompare(b[0]) < 0) a.shift();
+		else if (a[0].localeCompare(b[0]) > 0) b.shift();
+		else { r = true; break; }
+	}
+	return r;
 }
 
 function getOptions() {
-	chrome.storage.sync.get("iDunLikeREs", function(items) {
+	chrome.storage.sync.get({"iDunLikeREs": [], "iDunLikeREsLikey": []} , function(items) {
 		if (items["iDunLikeREs"].length !== 0) {
 			items["iDunLikeREs"].sort();
 			for (var i = 0; i < items["iDunLikeREs"].length; i++) {
 				addToList(items["iDunLikeREs"][i]);
 			}
 		}
+    if (items["iDunLikeREsLikey"].length !== 0) {
+			items["iDunLikeREsLikey"].sort();
+			for (var i = 0; i < items["iDunLikeREsLikey"].length; i++) {
+				addToListLikey(items["iDunLikeREsLikey"][i]);
+			}
+		}
 	});
 }
 
 function addButtonFunctions() {
-	for (var d = 0; d < document.querySelectorAll('li').length; d++) {
-		document.querySelectorAll('button')[d * 2].addEventListener('click', editElement);
-		document.querySelectorAll('button')[d * 2 + 1].addEventListener('click', removeElement);
-	}
+	for (var d = 0; d < document.querySelectorAll('.edt-no-likey').length; d++)
+		document.querySelectorAll('.edt-no-likey')[d].addEventListener('click', editElement);
+
+  for (var d = 0; d < document.querySelectorAll('.rmv-no-likey').length; d++)
+  	document.querySelectorAll('.rmv-no-likey')[d].addEventListener('click', removeElement);
+
+  for (var d = 0; d < document.querySelectorAll('.edt-likey').length; d++)
+		document.querySelectorAll('.edt-likey')[d].addEventListener('click', editElementLikey);
+
+  for (var d = 0; d < document.querySelectorAll('.rmv-likey').length; d++)
+  	document.querySelectorAll('.rmv-likey')[d].addEventListener('click', removeElement);
 }
+
 
 document.addEventListener("DOMContentLoaded", function() {
 	if(document.querySelectorAll('button') !== null) {
@@ -62,10 +128,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.addEventListener("DOMNodeInserted", addButtonFunctions);
 
-document.getElementById("submit").addEventListener('click', addRegex);
-document.getElementById("regex").addEventListener('keypress', function(e) {
+document.getElementById("submit-no-likey").addEventListener('click', addRegex);
+document.getElementById("regex-no-likey").addEventListener('keypress', function(e) {
 	if (e.keyCode == 13) {
 		addRegex();
 	}
 });
-document.getElementById("save").addEventListener('click', saveOptions);
+document.getElementById("submit-likey").addEventListener('click', addRegexLikey);
+document.getElementById("regex-likey").addEventListener('keypress', function(e) {
+	if (e.keyCode == 13) {
+		addRegexLikey();
+	}
+});
+
+document.getElementById("save-changes").addEventListener('click', saveOptions);
